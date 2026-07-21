853c9c42c06544a0806e38b40a7dc1d1c83a7bd8
skyseySa <zenyokneupokoev@gmail.com>
2026-07-22 02:42:23 +0300

    Implement basic Telegram bot with message forwarding

---
+++ Bot.py
@@ -0,0 +1,46 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+TOKEN = "8533727565:AAEwr8LkVsz3b8SW467F9aJWi_RST8oyKtg"
+ADMIN_ID = 6254922733
+
+bot = Bot(TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(
+        "👋 Привет!\n"
+        "Напиши мне сообщение, и я передам его владельцу."
+    )
+
+
+@dp.message()
+async def messages(message: Message):
+    if message.chat.id == ADMIN_ID:
+        return
+
+    username = (
+        f"@{message.from_user.username}"
+        if message.from_user.username
+        else "Нет username"
+    )
+
+    await bot.send_message(
+        ADMIN_ID,
+        f"""📩 Новое сообщение
+
+👤 Имя: {message.from_user.full_name}
+🔗 Username: {username}
+🆔 ID: {message.from_user.id}
+"""
+    )
+
+    await bot.forward_message(
+        chat_id=ADMIN_ID,
+        from_chat_id=message.chat.id,
+        message_id=message.message_id,
+    )
